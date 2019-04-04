f41782655110d8929b47f47a0fb11c823d75440b
Eduardo San Martin Morote <posva13@gmail.com>
2019-04-04 10:26:04 +0200

    add edge

--- karma.conf.js
+++ karma.conf.js
@@ -1,44 +1,36 @@
 // Karma configuration
 // Generated on Fri Aug 10 2018 16:05:52 GMT+0200 (Central European Summer Time)
 
-let browsers = ['Chrome']
+let browsers = ['ChromeHeadless']
 
 if (process.env.CIRCLECI) {
   browsers = ['Chrome', 'Firefox']
 } else if (process.env.CI_WINDOWS) {
-  browsers = ['IE']
+  browsers = ['IE', 'Edge']
 } else if (process.env.TRAVIS) {
   browsers = ['Safari']
 }
 
 module.exports = function(config) {
   config.set({
-
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-
     // list of files / patterns to load in the browser
-    files: [
-      { pattern: 'test/**/*.spec.js', watched: false }
-    ],
-
+    files: [{ pattern: 'test/**/*.spec.js', watched: false }],
 
     // list of files / patterns to exclude
-    exclude: [
-    ],
-
+    exclude: [],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'src/**/*.js': ['webpack', 'sourcemap'],
-      'test/**/*.js': ['webpack', 'sourcemap']
+      'test/**/*.js': ['webpack', 'sourcemap'],
     },
 
     webpack: {
@@ -52,12 +44,12 @@ module.exports = function(config) {
             use: {
               loader: 'babel-loader',
               options: {
-                presets: ['@babel/preset-env']
-              }
-            }
-          }
-        ]
-      }
+                presets: ['@babel/preset-env'],
+              },
+            },
+          },
+        ],
+      },
     },
 
     // test results reporter to use
@@ -65,29 +57,23 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     // browsers: ['Chrome', 'Firefox', 'Opera', 'Safari'],
     browsers,
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -95,6 +81,6 @@ module.exports = function(config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: Infinity
+    concurrency: Infinity,
   })
 }
